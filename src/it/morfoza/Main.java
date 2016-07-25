1a077907fbad277142e047ba9c9f15baa95d7cfe
Wojt <ciechman.ws@gmail.com>
2016-07-25 15:59:45 +0200

    Four operations and results

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -6,11 +6,39 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int result = 25600*25600;
-        System.out.println("Hello! What's your name?");
+        System.out.println("Do you want to:");
+        System.out.println("1.Add");
+        System.out.println("2.Substract");
+        System.out.println("3.Multiply");
+        System.out.println("4.Divide");
         Scanner input = new Scanner (System.in);
         String name = input.nextLine();
-        System.out.println(name + ", your result is : " + result);
+
+            System.out.println("How much is x:");
+            int x = input.nextInt();
+            System.out.println("How much is y:");
+            int y = input.nextInt();
+
+            if (name.equals("1")) {
+                int result = x + y;
+                System.out.println("Your result of adding " + x + " to " + y + " is : " + result);
+            }
+            if (name.equals("2")) {
+                int result = x - y;
+                System.out.println("Your result of substracting " + x + " from " + y + " is : " + result);
+            }
+            if (name.equals("3")) {
+                int result = x * y;
+                System.out.println("Your result of multiplying " + x + " by " + y + " is : " + result);
+            }
+            if (name.equals("4")) {
+                int result = x / y;
+                System.out.println("Your result of dividing " + x + " by " + y + " is : " + result);
+            }
+
 
     }
 }
+
+
+
