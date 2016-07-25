ca26f26eb0a9fe5cdfc044345f4013f59d7ae467
Wojt <ciechman.ws@gmail.com>
2016-07-25 11:39:48 +0200

    Start projektu!

---
+++ src/it/morfoza/Main.java
@@ -0,0 +1,10 @@
+package it.morfoza;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hejeczka");
+        System.out.println("Falafel");
+    }
+}
