fd32ee10d5be75411a0a7e5acbe2a9a0413de28e
Wojt <ciechman.ws@gmail.com>
2016-07-25 11:45:11 +0200

    English

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hejeczka");
-        System.out.println("Zjedz falafel");
+        System.out.println("Hello");
+        System.out.println("Eat a falafel");
         System.out.println("Kupito");
     }
 }
