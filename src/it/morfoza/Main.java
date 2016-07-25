bad917830fe16bba39936749e977d57c0e0b654d
Wojt <ciechman.ws@gmail.com>
2016-07-25 11:44:22 +0200

    Dodajemy

--- src/it/morfoza/Main.java
+++ src/it/morfoza/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hejeczka");
-        System.out.println("Falafel");
+        System.out.println("Zjedz falafel");
         System.out.println("Kupito");
     }
 }
